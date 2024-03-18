44e6517743b6a7b651889d09dee96369a631995f
Łukasz Wieczorek <wieczorek1990@gmail.com>
2024-03-18 14:10:37 +0100

    Initial commit.

---
+++ prayers/prayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  prayersApp.swift
+//  prayers
+//
+//  Created by Łukasz Wieczorek on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct prayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
